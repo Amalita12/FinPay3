4d6dd87cb48a8740690ac9432576e66783099ffa
hayat <lahlalihayat70@gmail.com>
2026-02-23 14:51:58 +0000

    test

---
+++ src/test/java/com/finpay3/FactureDAOTest.java
@@ -0,0 +1,7 @@
+package com.finpay3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactureDAOTest {
+
+}
---
+++ src/test/java/com/finpay3/PaymentDAOTest.java
@@ -0,0 +1,8 @@
+package com.finpay3;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentDAOTest {
+
+
+}
