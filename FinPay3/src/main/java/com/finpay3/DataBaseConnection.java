1ed2633d5ec6ce5d8c9522be0f124d3849f1aa5f
Amal <amalbasbas55@gmail.com>
2026-02-23 12:43:59 +0000

    initial files

---
+++ FinPay3/src/main/java/com/finpay3/DataBaseConnection.java
@@ -0,0 +1,25 @@
+package com.finpay3;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/finpay_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "amalbasbas123456";
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/finpay_db", "root", "amalbasbas123456");
+        } catch (SQLException e) {
+            System.err.println("Connection failed! " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
+
